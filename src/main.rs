9de1c3932ddc41ff7484066f3e54cb9c45249806
Michael Terrell <cwithmichael@gmail.com>
2022-04-18 16:48:47 -0500

    cleaning up main function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 mod file_type;
+use clap::StructOpt;
 use std::io::stdin;
 use std::process;
 use zip_blitz::{Args, Config};
 
 fn main() {
-    let args = <Args as clap::StructOpt>::parse();
+    let args = Args::parse();
     let config = Config::new(args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-    let stdin = stdin();
-    if let Err(e) = zip_blitz::run(config, stdin) {
-        println!("Failure: {}", e);
-        process::exit(1);
+
+    match zip_blitz::run(config, stdin().lock()) {
+        Ok(password) => println!("Found it: {}", password),
+        Err(e) => {
+            println!("Failure: {}", e);
+            process::exit(1);
+        }
     }
 }
